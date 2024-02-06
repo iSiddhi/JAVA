5330fec00eec2ed834b9aa96078a51af8fb8c6c1
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-06 22:07:34 +0530

    Create 02_String.java

---
+++ 02_String.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        String greeting ="Hello ";
+                System.out.println(greeting);
+String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println("The length of the txt string is: " + txt.length());
+
+
+    }
+}
