a22b260869269edf024b49d16dc94f145adffd47
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-06 21:45:34 +0530

    Create 01Hello.java

---
+++ 01Hello.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int x=9;
+        double myDouble=x;
+        System.out.println(x);
+        System.out.println(myDouble);
+        
+        
+    }
+}
+
+
+public class Main{
+    public static void main(String[] args){
+        double myDouble=9.78d;
+        int myInt=(int) myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
