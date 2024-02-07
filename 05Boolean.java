a515049c2056bf72ecf03e94e8c636c0b46940c9
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-07 21:56:23 +0530

    Create 05Boolean.java

---
+++ 05Boolean.java
@@ -0,0 +1,31 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        boolean isJavaFun= true;
+        boolean isDhoklaTasty= false;
+        System.out.println(isJavaFun);
+        System.out.println(DhoklaTasty);
+    }
+}
+
+class HelloWorld {
+    public static void main(String[] args) {
+int x = 10;
+System.out.println(x == 10);
+}}
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int myAge = 25;
+int votingAge = 18;
+
+if (myAge >= votingAge) {
+  System.out.println("Old enough to vote!");
+} else {
+  System.out.println("Not old enough to vote.");
+}
+    }
+}
