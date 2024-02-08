1610bca5b1f5acaf911763394c9dedfa2c21941d
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-08 22:35:06 +0530

    Create 06_ifelse.java

---
+++ 06_ifelse.java
@@ -0,0 +1,36 @@
+if (20 > 18) {
+  System.out.println("20 is greater than 18");
+}
+
+if (condition) {
+  // block of code to be executed if the condition is true
+} else {
+  // block of code to be executed if the condition is false
+}
+
+int time = 20;
+if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+// Outputs "Good evening."
+
+if (condition1) {
+  // block of code to be executed if condition1 is true
+} else if (condition2) {
+  // block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  // block of code to be executed if the condition1 is false and condition2 is false
+}
+
+
+int time = 22;
+if (time < 10) {
+  System.out.println("Good morning.");
+} else if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+// Outputs "Good evening."
