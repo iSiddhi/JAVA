7513d5ff96675ce1da54cfa3b840910208f99f02
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-10 22:35:06 +0530

    Create while loop.java

---
+++ while loop.java
@@ -0,0 +1,26 @@
+while (condition) {
+  // code block to be executed
+}
+
+
+
+int i = 0;
+while (i < 5) {
+  System.out.println(i);
+  i++;
+}
+
+
+
+
+do {
+  // code block to be executed
+}
+while (condition);
+
+int i = 0;
+do {
+  System.out.println(i);
+  i++;
+}
+while (i < 5);
