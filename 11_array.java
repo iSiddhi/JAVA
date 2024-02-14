0670755ed1bf9f09038ef625bfc15bdd0e997281
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-14 21:21:28 +0530

    Create 11_array.java

---
+++ 11_array.java
@@ -0,0 +1,15 @@
+String[] cars;
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+int[] myNum = {10, 20, 30, 40};
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+System.out.println(cars[0]);
+// Outputs Volvo
+cars[0] = "Opel";
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+cars[0] = "Opel";
+System.out.println(cars[0]);
+// Now outputs Opel instead of Volvo
+
+
+
+
