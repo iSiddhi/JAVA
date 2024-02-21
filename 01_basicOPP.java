95be511b23a59ef4b397da1e893dd3b31374ea2a
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-02-21 21:53:53 +0530

    Create 01_basicOPP.java

---
+++ 01_basicOPP.java
@@ -0,0 +1,39 @@
+public class Main {
+  int x = 5;
+}
+
+
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
+ 
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj1 = new Main();  // Object 1
+    Main myObj2 = new Main();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
+  }
+}
+
+
+public class Main {
+  int x = 5;
+}
+
+
+class Second {
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
+
+
