f9cd63f6597561faff14794596c9f57525eb88df
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-03-07 22:57:03 +0530

    Create oppss.java

---
+++ oppss.java
@@ -0,0 +1,14 @@
+public class Family {
+  int ageMALE= 5;
+  int ageFemale= 3;
+}
+
+
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
