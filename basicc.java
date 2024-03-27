d944123362b002d13eb8556465dedb39ce77dd54
Siddhi Patel <114800007+iSiddhi@users.noreply.github.com>
2024-03-27 23:07:28 +0530

    Create basicc.java

---
+++ basicc.java
@@ -0,0 +1,31 @@
+
+Online Java Compiler
+Programiz PRO ❯
+Main.java
+
+ Run 
+class HelloWorld {
+
+
+12345678
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code 
+    online
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+    }
+}
+Output
+ Clear 
+
+
+
+
+
+
+
+
+
+
+
